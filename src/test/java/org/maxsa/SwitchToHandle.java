89e51781276c9de1b6c222b66144cfaeaafcaecf
OrangeCoderRU <max.sablin@mail.ru>
2020-11-23 18:05:51 +0300

    Update SwitchToHandle.java

--- src/test/java/org/maxsa/SwitchToHandle.java
+++ src/test/java/org/maxsa/SwitchToHandle.java
@@ -2,15 +2,29 @@ package org.maxsa;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.sql.Time;
 import java.util.List;
+import java.util.Set;
 
-public class SwitchToHandle extends TestBase{
+public class SwitchToHandle extends TestBase {
 
     public boolean areElementsPresent(By locator) {
         return driver.findElements(locator).size() > 0;
+    }
+
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
     }
 
 
@@ -26,12 +40,22 @@ public class SwitchToHandle extends TestBase{
 
         driver.findElement(By.cssSelector("a.button")).click();
 
-        String original_handle = driver.getWindowHandle();
+        String original_window = driver.getWindowHandle();
+        Set <String> existingWindows = driver.getWindowHandles();
 
         List<WebElement> inputs = driver.findElements(By.cssSelector("form tr"));
 
-        inputs.get(1).findElement(By.cssSelector("a")).click();
-
+        for (WebElement link : inputs){
+            int current_child = inputs.indexOf(link) + 1;
+            if (areElementsPresent(By.cssSelector("form tr:nth-child(" + current_child + ") a"))){
+                link.findElement(By.cssSelector("a:not(#address-format-hint)")).click();
+                String newWindow = wait.until(anyWindowOtherThan(existingWindows));
+                driver.switchTo().window(newWindow);
+                driver.close();
+                driver.switchTo().window(original_window);
+            }
+        }
+        
         Thread.sleep(5000);
     }
 }
